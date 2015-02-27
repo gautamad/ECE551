9a5ce629e7142c1830728c344a61183b8b68e8be
Gautam Dhanabalan <gautam@tux-59.cae.wisc.edu>
2015-02-26 19:24:06 -0600

    PWM tb

--- src/PWM.v
+++ src/PWM.v
@@ -9,7 +9,7 @@ module PWM(clk, rst_n, duty, PWM_sig);
 	   count = 0;
            PWM_sig =  0;
 	end
-	else if (count < duty) begin
+	else if (count <= duty) begin           // Can Optimize here for AREA
 	   PWM_sig = 1;
 	   count = count + 1;
 	end
@@ -21,3 +21,20 @@ module PWM(clk, rst_n, duty, PWM_sig);
 endmodule
 	           
          
+module PWM_tb();
+     reg [9:0] duty;
+     reg clk, rst_n;
+     wire PWM_sig;
+
+PWM DUT_PWM(.duty(duty), .clk(clk), .rst_n(rst_n), .PWM_sig(PWM_sig));
+
+        initial begin
+	  #0 clk = 0;
+	  #0 rst_n = 0;
+	  #14 rst_n = 1;
+	  #15 duty = 1024; 	
+	end
+	
+	always @(clk)
+	  clk <= #5 ~clk;
+endmodule	
